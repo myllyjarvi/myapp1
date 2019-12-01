f285c16b18e954171dd718439b1283b20ae282c2
myllyjarvi <57672493+myllyjarvi@users.noreply.github.com>
2019-12-01 18:01:44 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,47 +1,43 @@
-$(document).ready(function(){
+$(document).ready(function() {
+ $("#list-items").html(localStorage.getItem("listItems"));
 
+ $(".add-items").submit(function(event) {
+   event.preventDefault();
 
-    $("#list-items").html(localStorage.getItem("listItems"));
+   var item = $("#todo-list-item").val();
 
-    $(".add-items").submit(function(event){
+   if (item) {
+     $("#list-items").append(
+       "<li><input class='checkbox' type='checkbox' />" +
+         item + 
+         " <a class='remove'>x</a><hr></li>");
 
-      event.preventDefault();
+     localStorage.setItem("listItems", $("#list-items").html());
+      $("#todo-list-item").val("");
+  }
+});
 
-      var item = $("#todo-list-item").val();
+$(document).on("change", ".checkbox", function() {
+  if ($(this).attr("checked")) {
+    $(this).removeAttr("checked");
+  } else {
+    $(this).attr("checked", "checked");
+  }
 
-      if (item){
-        $("#list-items").append(
-        "<li><input class='checkbox' type='checkbox' />" +
-          item + " <a class='remove'>x</a><hr></li>"
-        );
+  $(this)
+    .parent()
+    .toggleClass("completed");
 
-      localStorage.setItem("listItems", $("#list-items").html());
-        $("#todo-list-item").val("");
+  localStorage.setItem("listItem", $("#list-items").html());
+});
 
-
-      }
-
-    });
-
-
-    $(document).on("change", ".checkbox", function(){
-      if($(this).attr("checked")){
-        $(this).removeAttr("checked");
-      }
-      else{
-          $(this).attr("checked", "checked");
-      }
-
-      $(this).parent().toggleClass("completed");
-
-      localStorage.setItem("listItem", $("#list-items").html());
-
-    })
-
-    $(document).on("click", ".remove", function (){
-      $(this).parent().remove();
-      localStorage.setItem("listItem", $("#list-items").html());
-    });
-
-
+$(document).on("click", ".remove", function () {
+  $(this)
+    .parent()
+    .remove();
+  localStorage.setItem("listItem", $("#list-items").html());
   });
+});
+
+
+  
